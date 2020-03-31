420f360212f45a688da2d119f608c5bd2e229169
Ponmalar221 <62938461+Ponmalar221@users.noreply.github.com>
2020-03-31 16:49:49 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class Mobile {
+	
+	boolean isAndroid = true;
+	int num = 56;
+	String name = "Ponmalar";
+
+	public static void main(String[] args) {
+		System.out.println("am ready");	
+		
+		Mobile mob = new Mobile();
+		
+		boolean isDisplayed2 = mob.isAndroid;
+		System.out.println(isDisplayed2);
+		
+		int num2 = mob.num;
+		System.out.println(num2);
+		
+		
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
